23b025815cea4217cd3b68385678a94e022d82e8
Khemraj9815 <02230286.cst@rub.edu.bt>
2026-05-13 21:47:20 +0600

    testing the assertion message

--- test_app.py
+++ test_app.py
@@ -36,7 +36,7 @@ class TestHomeEndpoint:
         """Test that home endpoint returns correct message"""
         response = client.get('/')
         data = json.loads(response.data)
-        assert 'CI/CD Pipeline Demo App' in data['message']
+        assert 'This is CI/CD Pipeline demo app' in data['message']
 
 class TestErrorHandling:
     """Tests for error handling"""
